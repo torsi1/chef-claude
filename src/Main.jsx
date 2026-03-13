397223aa7ccb6dde6b784b05d9aca17cfba0464c
torsi1 <tothorsolya681@gmail.com>
2026-03-13 23:48:50 +0200

    Changes to be committed: 	modified:   public/index.html 	modified:   src/Main.jsx 	modified:   src/index.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,16 @@
 export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key = {ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(){
+        console.log("Form submitted!")
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
@@ -9,6 +18,9 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
